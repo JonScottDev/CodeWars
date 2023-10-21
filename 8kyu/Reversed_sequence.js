e0090c8c599073a6c67f3dbf913bec152781c86f
Jon Scott <jhscott12@gmail.com>
2023-10-21 18:49:42 -0400

    10/21 - Daily Codewars Solutions

---
+++ 8kyu/Reversed_sequence.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    let arr = []
+      let count = n
+      for(i=0; i<n; i++){
+          arr[i] = count
+          count--
+      }
+      return arr
+};
