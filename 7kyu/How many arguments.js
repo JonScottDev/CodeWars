3f5687f358c55d5a2cf245dc056942a4e835695a
Jon Scott <jhscott12@gmail.com>
2024-02-10 11:41:00 -0500

    2/10 - Daily Codewars Solutions

---
+++ 7kyu/How many arguments.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function that returns the number of arguments it received.
+
+// args_count() --> 0
+// args_count('a') --> 1
+// args_count('a', 'b') --> 2
+
+const args_count = (...arg) => arg.length;
