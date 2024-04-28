59060f21cd7a57dfef99039b7549b702cb2454d6
Jon Scott <jhscott12@gmail.com>
2024-04-28 18:39:00 -0400

    4/28 - Daily Codewars Solutions

--- js/8kyu/L1: Bartender, drinks!.js
+++ js/8kyu/L1: Bartender, drinks!.js
@@ -12,3 +12,22 @@
 // Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
 
 // Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+function getDrinkByProfession(param) {
+  switch (param.toLowerCase()) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
+}
